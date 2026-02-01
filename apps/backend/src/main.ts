6333bf9341a9fa52e2e8611604a33498cffa6371
marharita08 <rozgon.margarita08@gmail.com>
2026-02-01 16:30:01 +0200

    update cors settings

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: false,
   });
 
